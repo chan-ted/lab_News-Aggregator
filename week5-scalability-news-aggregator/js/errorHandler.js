2c02dd2fd73040e6fd1dff16f711765f8284df77
chan-ted <2460157@dundee.ac.uk>
2026-02-17 00:05:33 +0000

    move

--- week5-scalability-news-aggregator/js/errorHandler.js
+++
@@ -1,7 +0,0 @@
-export function showErrorMessage() {
-    const container = document.getElementById("newsContainer");
-    container.innerHTML = `
-        <p class="text-danger">Some news sources failed to load. Please try again.</p>
-        <button class="btn btn-warning" onclick="loadNews()">Retry</button>
-    `;
-}
